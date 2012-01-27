96fceb7edfd7f0e1c844d4f920cd1e9191230fbf
Rushi,Ganmukhi <rganmukh@litle.com>
2012-01-27 16:27:17 -0500

    deleted extra test files and updated veriosn in the rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,11 +1,11 @@
 require 'rake/gempackagetask'
 spec = Gem::Specification.new do |s| 
   s.name         = "LitleOnline"
-  s.summary      = "Ruby SDK produced by Litle & Co. for online transaction processing using Litle XML format v8.x"
+  s.summary      = "Ruby SDK produced by Litle & Co. for online transaction processing using Litle XML format v8.10"
   s.description  = File.read(File.join(File.dirname(__FILE__), 'DESCRIPTION'))
   s.requirements = 
       [ 'Contact  ClientSDKSupport@litle.com for more information' ]
-  s.version     = "1.8.8"
+  s.version     = "1.8.10"
   s.author      = "Litle & Co"
   s.email       = "RubySupport@litle.com"
   s.homepage    = "http://www.litle.com"
