9cf0d9128f814c3fad2f29804d8614f7888b01c9
Gregory,Dake <gdake@litle.com>
2012-02-08 10:30:59 -0500

    quote the version of schema

--- bin/Setup.rb
+++ bin/Setup.rb
@@ -43,13 +43,14 @@ class Setup
       puts "Please input your merchantId:"
       f.puts "currency_merchant_map:"
       f.puts "  DEFAULT: " + gets
-      f.puts	"version: 8.10"
+      f.puts	"version: '8.10'"
       puts "Please choose Litle url from the following list (example: 'cert') or directly input another URL: \nsandbox => https://www.testlitle.com/sandbox/vap/communicator/online \ncert => https://cert.litle.com/vap/communicator/online \nprecert => https://precert.litle.com/vap/communicator/online \nproduction1 => https://payment.litle.com/vap/communicator/online \nproduction2 => https://payment2.litle.com/vap/communicator/online"
       f.puts "url: " + Setup.choice(gets)
       puts "Please input the proxy address, if no proxy hit enter key: "
       f.puts	"proxy_addr: " + gets
       puts "Please input the proxy port, if no proxy hit enter key: "
       f.puts	"proxy_port: " + gets
+      f.puts "printxml: false"
     end
   end
 
