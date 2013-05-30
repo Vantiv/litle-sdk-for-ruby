96b504f07ead74803b04e0f806e48c8aea8860ce
brianarnold789 <barnold@litle.com>
2013-05-30 08:59:38 -0400

    updated path on sample

--- bin/sample_batch_driver.rb
+++ bin/sample_batch_driver.rb
@@ -49,21 +49,33 @@ updateCardHash = {
         'cardValidationNum'=>'123'
       }      
       
+accountUpdateHash = {
+        'reportGroup'=>'Planets',
+        'id'=>'12345',
+        'customerId'=>'0987',
+        'orderId'=>'1234',
+        'card'=>{
+        'type'=>'VI',
+        'number' =>'4100000000000001',
+        'expDate' =>'1210'
+      }}
+      
 path = Dir.pwd
       
 
 request = LitleOnline::LitleRequest.new({'sessionId'=>'8675309'})
 request.create_new_litle_request(path)
 puts "Created new LitleRequest at location: " + path
-
+start = Time::now
 #create five batches, each with 10 sales
-2.times{
+5.times{
   batch = LitleOnline::LitleBatchRequest.new
   batch.create_new_batch(path)
 
   #add the same sale ten times
-  3.times{
-    batch.update_card_validation_num_on_token(updateCardHash)
+  10.times{
+    #batch.account_update(accountUpdateHash)
+    batch.sale(saleHash)
   }
 
   #close the batch, indicating we plan to add no more transactions
@@ -80,8 +92,10 @@ puts "Generated final XML markup of the LitleRequest"
 request.send_to_litle
 puts "Dropped off the XML of the LitleRequest over FTP"
 #grab the expected number of responses from the sFTP server and save them to the given path
-request.get_responses_from_server(1)
+request.get_responses_from_server()
 puts "Received the LitleRequest responses from the server"
+stop = Time::now
+puts "Total time: " + (stop - start).to_s  
 #process the responses from the server with a listener which applies the given block
 request.process_responses({:transaction_listener => LitleOnline::DefaultLitleListener.new do |transaction|
   puts transaction["type"]
