0dff27bec1e25d3e1ada8fffd8ff78e7b037d990
Gregory,Dake <gdake@litle.com>
2013-06-06 09:49:05 -0400

    making test pass in jenkins

--- test/functional/test_batch.rb
+++ test/functional/test_batch.rb
@@ -22,7 +22,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 =end
-require '../../lib/LitleOnline'
+require 'lib/LitleOnline'
 require 'test/unit'
 
 #test Authorization Transaction
