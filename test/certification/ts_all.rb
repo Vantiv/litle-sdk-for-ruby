7bf71f67c432eb99ae712c0458bdeae0ad0dca93
Aaron, Hammond <ahammond@litle.com>
2013-06-12 16:25:39 -0400

    Attempt to fix test driver

--- test/certification/ts_all.rb
+++ test/certification/ts_all.rb
@@ -22,6 +22,8 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 =end
+require 'lib/LitleOnline'
+
 
 #test driver for running all tests
 require_relative 'certTest1_base'
