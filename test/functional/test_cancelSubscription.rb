fba88f41380baf9f844a8379d45a7ae133c6e1d2
pshah91 <pshah91@ccs.neu.edu>
2014-03-13 17:02:12 -0400

    adding functional test (8.21)

---
+++ test/functional/test_cancelSubscription.rb
@@ -0,0 +1,55 @@
+=begin
+Copyright (c) 2012 Litle & Co.
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+=end
+require 'lib/LitleOnline'
+require 'test/unit'
+
+#test Authorization Transaction
+module LitleOnline
+ class TestCancelSubscription < Test::Unit::TestCase
+  def test_simple_happy
+    hash = {
+        'merchantId' => '101',
+        'version'=>'8.8',
+        'reportGroup'=>'Planets',
+        'subscriptionId' =>'1001'  
+	   }
+
+    response= LitleOnlineRequest.new.cancel_subscription(hash)
+    assert_equal('Valid Format', response.message)
+  end
+
+  def test_simple_error
+    hash = {
+        'merchantId' => '101',
+        'version'=>'8.8',
+        'reportGroup'=>'Planets',
+	   }
+
+    response= LitleOnlineRequest.new.cancel_subscription(hash)
+    assert(response.message =~ /Error validating xml data against the schema/)
+  end
+ 
+ end
+end
