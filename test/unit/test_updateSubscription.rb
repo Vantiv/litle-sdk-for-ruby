810dae23847fec81bf441a469b8148ed17c44a9f
pshah91 <pshah91@ccs.neu.edu>
2014-03-13 11:01:37 -0400

    adding unit test

---
+++ test/unit/test_updateSubscription.rb
@@ -0,0 +1,65 @@
+=begin
+Copyright (c) 2012 Litle & Co.
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+=end
+require 'lib/LitleOnline'
+require 'test/unit'
+
+module LitleOnline
+  class TestUpdateSubscription < Test::Unit::TestCase
+
+    def test_simple
+      hash = {
+	'subscriptionId' =>'100',
+        'merchantId' => '101',
+        'version'=>'8.8',
+      }
+      LitleXmlMapper.expects(:request).with(regexp_matches(/.*<updateSubscription><subscriptionId>100<\/subscriptionId><\/updateSubscription>.*/m), is_a(Hash))
+      LitleOnlineRequest.new.update_subscription(hash)
+    end
+
+    def test_allFields
+      hash = {
+	'subscriptionId' =>'100',
+        'merchantId' => '101',
+        'version'=>'8.8',
+        'reportGroup'=>'Planets',
+        'planCode' => 'planCodeString',
+        'billToAddress' =>  
+                {
+                 'name' =>'nameString'               
+                },
+        'card'=>
+                {  
+                 'type'=>'VI',
+                 'number' =>'4100000000000001',
+                 'expDate' =>'1210'
+                },
+        'billingDate' =>'2014-03-11'        
+            } 
+      LitleXmlMapper.expects(:request).with(regexp_matches(/.*<updateSubscription><subscriptionId>100<\/subscriptionId><planCode>planCodeString<\/planCode><billToAddress><name>nameString<\/name><\/billToAddress><card><type>VI<\/type><number>4100000000000001<\/number><expDate>1210<\/expDate><\/card><billingDate>2014-03-11<\/billingDate><\/updateSubscription>.*/m), is_a(Hash))
+      LitleOnlineRequest.new.update_subscription(hash)
+    end 
+   end
+
+end
