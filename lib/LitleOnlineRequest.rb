0a02b6d280cb01ce61c61d8e20a8627e3c0bc520
pshah91 <pshah91@ccs.neu.edu>
2014-03-14 17:04:18 -0400

    == Version 8.23.0 (March 14, 2014)

--- lib/LitleOnlineRequest.rb
+++ lib/LitleOnlineRequest.rb
@@ -252,7 +252,7 @@ module LitleOnline
 
       request.authentication  = authentication
       request.merchantId      = get_merchant_id(options)
-      request.version         = '8.22'
+      request.version         = '8.23'
       request.loggedInUser    = get_logged_in_user(options)
       request.xmlns           = "http://www.litle.com/schema"
       request.merchantSdk     = get_merchant_sdk(options)
@@ -283,7 +283,7 @@ module LitleOnline
     end
 
     def get_merchant_sdk(options)
-      options['merchantSdk'] || 'Ruby;8.22.0'
+      options['merchantSdk'] || 'Ruby;8.23.0'
     end
 
     def get_report_group(options)
