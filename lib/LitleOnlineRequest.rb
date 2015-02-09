355482cdd5b7e7b95df2caf8588d650918f55974
leonli0326 <leonli0326@gmail.com>
2015-02-09 16:38:16 -0500

    updated version

--- lib/LitleOnlineRequest.rb
+++ lib/LitleOnlineRequest.rb
@@ -258,7 +258,7 @@ module LitleOnline
 
       request.authentication  = authentication
       request.merchantId      = get_merchant_id(options)
-      request.version         = '8.29'
+      request.version         = '9.3'
       request.loggedInUser    = get_logged_in_user(options)
       request.xmlns           = "http://www.litle.com/schema"
       request.merchantSdk     = get_merchant_sdk(options)
@@ -289,7 +289,7 @@ module LitleOnline
     end
 
     def get_merchant_sdk(options)
-      options['merchantSdk'] || 'Ruby;8.29.0'
+      options['merchantSdk'] || 'Ruby;9.3.0'
     end
 
     def get_report_group(options)
--- lib/LitleRequest.rb
+++ lib/LitleRequest.rb
@@ -191,7 +191,7 @@ module LitleOnline
       litleRequest.authentication = authentication
       litleRequest.numBatchRequests = "0"
       
-      litleRequest.version         = '8.29'
+      litleRequest.version         = '9.3'
       litleRequest.xmlns           = "http://www.litle.com/schema"
       
       
@@ -499,7 +499,7 @@ module LitleOnline
       authentication.password = get_config(:password, options)
 
       litle_request.authentication = authentication
-      litle_request.version         = '8.29'
+      litle_request.version         = '9.3'
       litle_request.xmlns           = "http://www.litle.com/schema"
       # litle_request.id              = options['sessionId'] #grab from options; okay if nil
       litle_request.numBatchRequests = @num_batch_requests
