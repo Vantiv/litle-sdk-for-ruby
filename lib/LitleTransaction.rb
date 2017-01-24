9abdb313982d5d8d57bed533c0f88d13df988c4c
Zhilong Hou <zhilong.hou@vantiv.com>
2017-01-24 13:29:37 -0500

    rebranding.

--- lib/LitleTransaction.rb
+++ lib/LitleTransaction.rb
@@ -1,5 +1,5 @@
 =begin
-Copyright (c) 2011 Litle & Co.
+Copyright (c) 2017 Vantiv eCommerce
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
 files (the "Software"), to deal in the Software without
