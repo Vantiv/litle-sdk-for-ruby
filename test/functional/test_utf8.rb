af0f4ed97b51a2c27dd33115f2604c65b7e36dd5
Houzl <zhilonghou@gmail.com>
2017-03-17 11:13:26 -0400

    add utf8 test

---
+++ test/functional/test_utf8.rb
@@ -0,0 +1,44 @@
+=begin
+Copyright (c) 2017 Vantiv eCommerce
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+=end
+require File.expand_path("../../../lib/LitleOnline",__FILE__) 
+require 'test/unit'
+
+#test ActivateReversal Transaction
+module LitleOnline
+ class TestActivateReversal < Test::Unit::TestCase
+  
+def test_simple
+    hash = {
+        'merchantId' => '101',
+        'version'=>'8.8',
+        'reportGroup'=>'русский中文',
+        'litleTxnId' =>'5000'
+	   }
+
+    response= LitleOnlineRequest.new.activate_reversal(hash)
+    assert_equal('Valid Format', response.message)
+  end
+ end
+end
