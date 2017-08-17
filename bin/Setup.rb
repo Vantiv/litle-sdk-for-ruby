c4985f5a533a9030293405bd8465c66e6b7622a5
Brian Arnold <sdksupport@litle.com>
2017-08-17 07:43:02 -0400

    Update URL to be current, start fixing Ruby style

--- bin/Setup.rb
+++ bin/Setup.rb
@@ -18,7 +18,7 @@ included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
 HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
@@ -34,39 +34,35 @@ class Setup
   attr_reader :handle, :path
   def initialize(filename)
     @handle = File.new(filename, File::CREAT|File::TRUNC|File::RDWR, 0600)
-    File.open(filename, "w") do |f|
-      puts "Welcome to Vantiv eCommerce Ruby_SDK"
-      puts "Please input your user name:"
-      f.puts  "user: "+ gets
-      puts "Please input your password:"
-      f.puts	"password: " + gets
-      puts "Please input your merchantId:"
-      
-      f.puts "currency_merchant_map:"
-      f.puts "  DEFAULT: " + gets
+    File.open(filename, 'w') do |f|
+      puts 'Welcome to Vantiv eCommerce Ruby_SDK'
+      puts 'Please input your user name:'
+      f.puts  'user: '+ gets
+      puts 'Please input your password:'
+      f.puts 'password: ' + gets
+      puts 'Please input your merchantId:'
+      f.puts 'currency_merchant_map:'
+      f.puts '  DEFAULT: ' + gets
       f.puts "default_report_group: 'Default Report Group'"
-      
-      puts "Please choose Vantiv eCommerce url from the following list (example: 'prelive') or directly input another URL: \nsandbox => https://www.testlitle.com/sandbox/communicator/online \nprelive => https://prelive.litle.com/vap/communicator/online \npostlive => https://postlive.litle.com/vap/communicator/online \nproduction => https://payments.litle.com/vap/communicator/online \ntransact_prelive => https://transact-prelive.litle.com/vap/communicator/online \ntransact_postlive => https://transact-postlive.litle.com/vap/communicator/online \ntransact_production => https://transact.litle.com/vap/communicator/online \ntransact_betacert => https://transact-betacert.litle.com/vap/communicator/online \nbetacert => https://betacert.litle.com/vap/communicator/online"
-      f.puts "url: " + Setup.choice(gets)
-      puts "Please input the proxy address, if no proxy hit enter key: "
-      f.puts	"proxy_addr: " + gets
-      puts "Please input the proxy port, if no proxy hit enter key: "
-      f.puts	"proxy_port: " + gets
-      puts "Please input your sFTP username for batch processing; if no sFTP, hit enter key: "
-      f.puts "sftp_username: " + gets
-      puts "Please input your sFTP password for batch processing; if no sFTP, hit enter key: "
-      f.puts "sftp_password: " + gets
-      puts "Please input your sFTP url for batch processing; if no sFTP, hit enter key: "
-      f.puts "sftp_url: " + gets
-      puts "Please input your url for fast batch processing; if no fast batch, hit enter key: "
-      f.puts "fast_url: "  + gets
-      puts "Please input your port for fast batch processing; if no fast batch, hit enter key: "
-      f.puts "fast_port: "  + gets
-      
-      f.puts "printxml: false"
-      #default http timeout set to 500 ms
-      f.puts "timeout: 500"
-      
+      puts "Please choose Vantiv eCommerce url from the following list (example: 'prelive') or directly input another URL: \nsandbox => https://www.testvantivcnp.com/sandbox/communicator/online \nprelive => https://payments.vantivprelive.com/vap/communicator/online \npostlive => https://payments.vantivpostlive.com/vap/communicator/online \nproduction => https://payments.vantivcnp.com/vap/communicator/online \ntransact_prelive => https://transact.vantivprelive.com/vap/communicator/online \ntransact_postlive => https://transact.vantivpostlive.com/vap/communicator/online \ntransact_production => https://transact.vantivcnp.com/vap/communicator/online"
+      f.puts 'url: ' + Setup.choice(gets)
+      puts 'Please input the proxy address, if no proxy hit enter key: '
+      f.puts 'proxy_addr: ' + gets
+      puts 'Please input the proxy port, if no proxy hit enter key: '
+      f.puts 'proxy_port: ' + gets
+      puts 'Please input your sFTP username for batch processing; if no sFTP, hit enter key: '
+      f.puts 'sftp_username: ' + gets
+      puts 'Please input your sFTP password for batch processing; if no sFTP, hit enter key: '
+      f.puts 'sftp_password: ' + gets
+      puts 'Please input your sFTP url for batch processing; if no sFTP, hit enter key: '
+      f.puts 'sftp_url: ' + gets
+      puts 'Please input your url for fast batch processing; if no fast batch, hit enter key: '
+      f.puts 'fast_url: ' + gets
+      puts 'Please input your port for fast batch processing; if no fast batch, hit enter key: '
+      f.puts 'fast_port: ' + gets
+      f.puts 'printxml: false'
+      # default http timeout set to 500 ms
+      f.puts 'timeout: 500'
     end
   end
 
@@ -77,25 +73,21 @@ class Setup
   def Setup.choice(litle_env)
     litle_online_ctx = 'vap/communicator/online'
     if litle_env == "sandbox\n"
-      return 'https://www.testlitle.com/sandbox/communicator/online'
+      return 'https://www.testvantivcnp.com/sandbox/communicator/online'
     elsif litle_env == "prelive\n"
-      return 'https://prelive.litle.com/' + litle_online_ctx
+      return 'https://payments.vantivprelive.com/' + litle_online_ctx
     elsif litle_env == "postlive\n"
-      return 'https://postlive.litle.com/' + litle_online_ctx
-    elsif litle_env == "betacert\n"
-      return 'https://betacert.litle.com/' + litle_online_ctx
+      return 'https://payments.vantivpostlive.com/' + litle_online_ctx
     elsif litle_env == "production\n"
-      return 'https://payments.litle.com/' + litle_online_ctx
+      return 'https://payments.vantivcnp.com/' + litle_online_ctx
     elsif litle_env == "transact_production\n"
-      return 'https://transact.litle.com/' + litle_online_ctx
+      return 'https://transact.vantivcnp.com/' + litle_online_ctx
     elsif litle_env == "transact_prelive\n"
-      return 'https://transact-prelive.litle.com/' + litle_online_ctx
+      return 'https://transact.vantivprelive.com/' + litle_online_ctx
     elsif litle_env == "transact_postlive\n"
-      return 'https://transact-postlive.litle.com/' + litle_online_ctx
-    elsif litle_env == "transact_betacert\n"
-      return 'https://transact-betacert.litle.com/' + litle_online_ctx
+      return 'https://transact.vantivpostlive.com/' + litle_online_ctx
     else
-      return 'https://www.testlitle.com/sandbox/communicator/online'
+      return 'https://www.testvantivcnp.com/sandbox/communicator/online'
     end
   end
 end
@@ -115,11 +107,11 @@ end
 
 # make the config.yml file hidden
 # create a config file contain all the configuration data
-config_file = path + "/.litle_SDK_config.yml"
+config_file = path + '/.litle_SDK_config.yml'
 f = Setup.new(config_file)
 
 # return the path of the config file and the path file
 @path = File.expand_path(config_file)
-puts "The Vantiv eCommerce configuration file has been generated, the file is located at: " + @path
+puts 'The Vantiv eCommerce configuration file has been generated, the file is located at: ' + @path
 f.finished
 
